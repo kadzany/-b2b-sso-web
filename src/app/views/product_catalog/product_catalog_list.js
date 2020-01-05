d655a020938eba76ce2a8a3885a689d730f39552
teguh.noor <teguh.noor@gmail.com>
2020-01-05 23:13:00 +0700

    onprogress product list

---
+++ src/app/views/product_catalog/product_catalog_list.js
@@ -0,0 +1,16 @@
+    $(document).ready(function () {
+        const productCatalogListUrl = 'https://apibisnis.blanja.com/api/v1/catalog/categories/40/products?limit=8&offset=8&sort=-created_at';
+        window.parent.api.get(productCatalogListUrl).then(function (res) {
+            if (res && res.data && res.data.data) {
+                var dataSource = new kendo.data.DataSource({
+                    data: res.data.data,
+                    pageSize: 20
+                });
+    
+                $("#listView").kendoListView({
+                    dataSource: dataSource,
+                    template: kendo.template($("#productTemplate").html())
+                });
+            }
+        });
+    });
