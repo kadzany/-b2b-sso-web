7172437890f139c6ee6f726f8039a66aabfe6cb0
Dita Aprilani <ditapril@gmail.com>
2020-01-08 15:08:49 +0700

    update grid type

--- src/js/request_product.js
+++ src/js/request_product.js
@@ -1,63 +1,68 @@
 (function(){
     $(document).ready(function () {
-        var crudServiceBaseUrl = "https://demos.telerik.com/kendo-ui/service",
-            dataSource = new kendo.data.DataSource({
+        $("#grid").kendoGrid({
+            dataSource: {
+                batch: true,
+                type: "odata-v4",
                 transport: {
-                    read:  {
-                        url: crudServiceBaseUrl + "/Products",
-                        dataType: "jsonp"
+                    read: {
+                        url: function () {
+                            return "https://demos.telerik.com/kendo-ui/service-v4/odata/Products";
+                        }
                     },
                     update: {
-                        url: crudServiceBaseUrl + "/Products/Update",
-                        dataType: "jsonp"
+                        url: function (dataItem) {
+                            return "https://demos.telerik.com/kendo-ui/service-v4/odata/Products(" + dataItem.ProductID + ")";
+                        }
                     },
-                    destroy: {
-                        url: crudServiceBaseUrl + "/Products/Destroy",
-                        dataType: "jsonp"
+                    batch: {
+                        url: function () {
+                            return "https://demos.telerik.com/kendo-ui/service-v4/odata/$batch";
+                        }
                     },
                     create: {
-                        url: crudServiceBaseUrl + "/Products/Create",
-                        dataType: "jsonp"
+                        url: function (dataItem) {
+                            delete dataItem.ProductID;
+                            return "https://demos.telerik.com/kendo-ui/service-v4/odata/Products";
+                        }
                     },
-                    parameterMap: function(options, operation) {
-                        if (operation !== "read" && options.models) {
-                            return {models: kendo.stringify(options.models)};
+                    destroy: {
+                        url: function (dataItem) {
+                            return "https://demos.telerik.com/kendo-ui/service-v4/odata/Products(" + dataItem.ProductID + ")";
                         }
                     }
                 },
-                batch: true,
-                pageSize: 20,
                 schema: {
                     model: {
                         id: "ProductID",
                         fields: {
-                            ProductID: { editable: false, nullable: true },
+                            ProductID: { editable: false },
                             ProductName: { validation: { required: true } },
-                            Quantity: { type: "number", validation: { min: 0, required: true } },
-                            UnitPrice: { type: "number", validation: { required: true, min: 1} },
-                            ProductDescription: { validation: { required: true } }
+                            UnitPrice: { type: "number", validation: { required: true, min: 1 } },
+                            // Discontinued: { type: "boolean" },
+                            UnitsInStock: { type: "number", validation: { min: 0, required: true } }
                         }
                     }
-                }
-            });
-
-        $("#grid").kendoGrid({
-            dataSource: dataSource,
+                },
+                pageSize: 20
+            },
+            navigatable: true,
             pageable: true,
             height: 550,
-            toolbar: ["create"],
+            toolbar: ["create", "save", "cancel"],
             columns: [
                 "ProductName",
-                { field: "Quantity", title:"Quantity", width: "120px" },
-                { field: "UnitPrice", title: "Unit Price", format: "{0:c}", width: "120px" },
-                "ProductDescription",
-                { command: ["edit", "destroy"], title: "&nbsp;", width: "250px" }],
-            editable: "inline"
+                { field: "UnitPrice", title: "Unit Price", format: "{0:c}", width: 120 },
+                { field: "UnitsInStock", title: "Units In Stock", width: 120 },
+                // { field: "Discontinued", width: 120, editor: customBoolEditor },
+                { command: "destroy", title: "&nbsp;", width: 150 }],
+            editable: true
         });
     });
 
     function customBoolEditor(container, options) {
         var guid = kendo.guid();
+        $('<input class="k-checkbox" id="' + guid + '" type="checkbox" name="Discontinued" data-type="boolean" data-bind="checked:Discontinued">').appendTo(container);
         $('<label class="k-checkbox-label" for="' + guid + '">&#8203;</label>').appendTo(container);
     }
 })();
