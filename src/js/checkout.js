b4fc278cbb439bd3e1706ef2bdca551a82231806
Eva Danti R <evadantir@gmail.com>
2020-01-09 10:23:02 +0700

    checkout page basic version

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,10 +1,11 @@
 (function(){
     $(document).ready(function() {
-        $("#grid").kendoGrid({
+        $("#grid-store-checkout").kendoGrid({
             dataSource: {
                 type: "odata",
+                pageSize: 3,
                 transport: {
-                    read: "https://demos.telerik.com/kendo-ui/service/Northwind.svc/Products"
+                    read: "https://demos.telerik.com/kendo-ui/service/Northwind.svc/Products",
                 },
                 schema:{
                     model: {
@@ -15,15 +16,89 @@
                         }
                     }
                 },
-                aggregate: [  { field: "UnitPrice", aggregate: "sum" },
-                              { field: "UnitsOnOrder", aggregate: "count" }]
+                aggregate: [  
+                    { field: "UnitPrice", aggregate: "sum" },
+                    { field: "UnitsOnOrder", aggregate: "sum"}]
             },
             sortable: false,
-            scrollable: true,
+            scrollable:true,
+            pageable: true,
             columns: [
-                { field: "ProductName", title: "Nama Produk"},
-                {field: "UnitPrice", title: "Harga per Unit", aggregates: ["sum"], footerTemplate: "Total Harga Barang: #=sum#", groupFooterTemplate: "Total Harga: #=sum#"  },
-                { field: "UnitsOnOrder", title: "Quantity", aggregates: ["count"], footerTemplate: "Jumlah Barang: #=count#", groupFooterTemplate: "Jumlah Barang: #=count#" }
+                { 
+                    field: "ProductName", 
+                    title: "Nama Produk",
+                    template: "<div class='product-photo'" +
+                    "style='background-image: url(../img/logo.jpg);'></div>" +
+                    "<div class='product-name'>#: ProductName #</div>"
+                },{
+                    field: "UnitPrice", 
+                    title: "Harga per Unit", 
+                    aggregates: ["sum"], 
+                    footerTemplate: "Sub Total: #=sum#"                    
+                },{
+                    field: "UnitsOnOrder", 
+                    title: "Quantity", 
+                    aggregates: ["sum"], 
+                    footerTemplate: "Jumlah Barang: #=sum#"
+                }
+            ]
+        });
+
+        $("#grid-request-checkout").kendoGrid({
+            dataSource: {
+                type: "odata",
+                pageSize: 3,
+                transport: {
+                    read: "https://demos.telerik.com/kendo-ui/service/Northwind.svc/Products",
+                },
+                schema:{
+                    model: {
+                        fields: {
+                            ProductName: { type: "string" },
+                            UnitPrice: { type: "number" },
+                            UnitsOnOrder: { type: "number" },
+                        }
+                    }
+                },
+                aggregate: [  
+                    { field: "UnitPrice", aggregate: "sum" },
+                    { field: "UnitsOnOrder", aggregate: "sum"}]
+            },
+            sortable: false,
+            scrollable:true,
+            pageable: true,
+            columns: [
+                { 
+                    field: "Num", 
+                    title: "Nomor"
+                },                
+                { 
+                    field: "Desc", 
+                    title: "Item Description",
+                    template: "<div class='product-photo'" +
+                    "style='background-image: url(../img/logo.jpg);'></div>" +
+                    "<div class='product-name'>#: ProductName #</div>"
+                },
+                {
+                    field: "UnitsOnOrder", 
+                    title: "Quantity", 
+                    aggregates: ["sum"], 
+                    footerTemplate: "Jumlah Barang: #=sum#"
+                },
+                { 
+                    field: "Unit", 
+                    title: "Satuan"
+                },
+                {
+                    field: "UnitPrice", 
+                    title: "Harga per Unit", 
+                    aggregates: ["sum"], 
+                    footerTemplate: "Sub Total: #=sum#"                    
+                },
+                { 
+                    field: "Remark", 
+                    title: "Remark"
+                },
             ]
         });
     });
