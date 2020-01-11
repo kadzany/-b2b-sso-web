5a6ccf631c077e93778efa798f29be048891f6cb
Khaerul Adzany <khaerul.adzany@gmail.com>
2020-01-11 12:00:06 +0700

    fix issue modal show

--- src/js/detail_po.js
+++ src/js/detail_po.js
@@ -1,64 +1,64 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     let po_data = [
         {
-            poNumber: 'PO1', createdDate:'01/01/2020', customer: 'Telkom', status: 'Approved', approver: "Admin SSO", fullfillmentDate: '03/01/2020',
-            poFromStore:[
-                { productName: "QUESO CABRALES", unitPrice: 1000, qty: 5,uom:'kg', remark:"Budget maksimal Rp 3.000.000,00" },
-                { productName: "ALICE MUTTON", unitPrice: 2000, qty: 7,uom:'kg',remark:"Budget maksimal Rp 3.000.000,00" },
-                { productName: "GENEN SHOUYU", unitPrice: 3000, qty: 3,uom:'kg',remark:"Budget maksimal Rp 3.000.000,00" },
-                { productName: "CHARTREUSE VERTE", unitPrice: 4000, qty: 1,uom:'kg',remark:"Budget maksimal Rp 3.000.000,00" }
+            poNumber: 'PO1', createdDate: '01/01/2020', customer: 'Telkom', status: 'Approved', approver: "Admin SSO", fullfillmentDate: '03/01/2020',
+            poFromStore: [
+                { productName: "QUESO CABRALES", unitPrice: 1000, qty: 5, uom: 'kg', remark: "Budget maksimal Rp 3.000.000,00" },
+                { productName: "ALICE MUTTON", unitPrice: 2000, qty: 7, uom: 'kg', remark: "Budget maksimal Rp 3.000.000,00" },
+                { productName: "GENEN SHOUYU", unitPrice: 3000, qty: 3, uom: 'kg', remark: "Budget maksimal Rp 3.000.000,00" },
+                { productName: "CHARTREUSE VERTE", unitPrice: 4000, qty: 1, uom: 'kg', remark: "Budget maksimal Rp 3.000.000,00" }
             ]
         },
         {
-            poNumber: 'PO2', createdDate:'01/01/2020', customer: 'Yakes', status: 'Rejected', approver: "admin SSO", fullfillmentDate: '03/01/2020'
+            poNumber: 'PO2', createdDate: '01/01/2020', customer: 'Yakes', status: 'Rejected', approver: "admin SSO", fullfillmentDate: '03/01/2020'
         },
         {
-            poNumber: 'PO3', createdDate:'01/01/2020', customer: 'Yakes', status: 'Requested', approver: "admin SSO", fullfillmentDate: '03/01/2020'
+            poNumber: 'PO3', createdDate: '01/01/2020', customer: 'Yakes', status: 'Requested', approver: "admin SSO", fullfillmentDate: '03/01/2020'
         }
     ];
 
     let request_schema = {
         model: {
-            num : {type:"number"},
+            num: { type: "number" },
             productName: { type: "string" },
-            uom: { type: "string"},
+            uom: { type: "string" },
             qty: { type: "number" },
-            unitPrice: { type: "number"},
-            remark: {type: "string"}         
+            unitPrice: { type: "number" },
+            remark: { type: "string" }
         },
         parse: function (data) {
             $.each(data, function (i) {
-                this.num = i+1;
+                this.num = i + 1;
             });
             return data;
         }
     };
 
     let request_columns = [
-        { 
-            field: "num", 
+        {
+            field: "num",
             title: "Nomor",
             width: 80
-        },                
-        { 
-            field: "productName", 
+        },
+        {
+            field: "productName",
             title: "Item Description",
         },
         {
-            field: "qty", 
+            field: "qty",
             title: "Quantity"
         },
-        { 
-            field: "uom", 
+        {
+            field: "uom",
             title: "Satuan"
         },
         {
-            field: "unitPrice", 
+            field: "unitPrice",
             title: "Harga per Unit"
         },
-        { 
-            field: "remark", 
+        {
+            field: "remark",
             title: "Remark"
         }
     ];
@@ -70,7 +70,7 @@ $(document).ready(function() {
         },
         editable: false,
         sortable: false,
-        scrollable:true,
+        scrollable: true,
         columns: request_columns
     });
 
@@ -81,7 +81,7 @@ $(document).ready(function() {
         },
         editable: false,
         sortable: false,
-        scrollable:true,
+        scrollable: true,
         columns: request_columns
     });
 
@@ -89,32 +89,4 @@ $(document).ready(function() {
     $("#approver").text(po_data[0].approver);
     $("#req-date").text(po_data[0].createdDate);
     $("#fullfill-date").text(po_data[0].fullfillmentDate);
-
 });
-
-let editor = $("#editor").kendoEditor().data("kendoEditor");
-
-let dialog=$("#editorDialog").kendoDialog({
-    width: "500px",
-    title: "Approve Purchase Order",
-    visible:false,
-    closable: true,
-    content: "<h3>Masukkan catatan</h3>",
-    actions: [
-        { text: 'Setujui', primary: true},
-        { text: 'Batalkan'}
-    ],
-    open: function(){
-        editor.refresh();
-    }
-}).data("kendoDialog");
-
-$("#approveBtn").on("click",function(){
-    console.log("hehe");
-    console.log(dialog);
-    dialog.open();
-});
-
-function updateText(){
-    $("#content").html(editor.value());
-};
