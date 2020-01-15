3bcf29851ed82c5fa1e043742d681a0b44ecfcd3
Khaerul Adzany <khaerul.adzany@gmail.com>
2020-01-15 16:15:21 +0700

    login milih role

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,32 @@
 (function () {
     $(document).ready(function () {
-        window.location = "/katalog_produk.html";
+
+        var userEmail = sessionStorage.getItem('user_email');
+        var userRole = sessionStorage.getItem('user_role');
+
+        console.log(userEmail);
+        console.log(userRole);
+
+        $(".item_menu").hide();
+
+        if(userRole == "admin"){
+            $("#vendor").show();
+            $("#katalog").show();
+            $("#purchasing").show();
+            $("#shopping_cart").show();
+            $("#kontrak").show();
+        }
+        else if(userRole == "vendor"){
+            $("#kontrak").show();
+            $("#vendor").show();
+            $("#katalog").show();
+        }
+        else if(userRole == "customer"){
+            $("#katalog").show();
+            $("#purchasing").show();
+            $("#shopping_cart").show();
+        }
+
+        //window.location = "/katalog_produk.html";
     });
 })();
