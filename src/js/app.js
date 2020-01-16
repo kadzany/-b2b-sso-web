0a1ae103db561757f660c8f2c157842080ba22f7
Khaerul Adzany <khaerul.adzany@gmail.com>
2020-01-16 12:31:14 +0700

    menu hide/show

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,34 @@
+// Menu selection
+
+  console.log('asdasdas');
+
+  var userEmail = sessionStorage.getItem('user_email');
+  var userRole = sessionStorage.getItem('user_role');
+
+  console.log(userEmail);
+  console.log(userRole);
+
+  $(".item_menu").hide();
+
+  if(userRole == "admin"){
+      $("#vendor").show();
+      $("#katalog").show();
+      $("#purchasing").show();
+      $("#shopping_cart").show();
+      $("#kontrak").show();
+      $("#user").show();
+  }
+  else if(userRole == "vendor"){
+      $("#kontrak").show();
+      $("#vendor").show();
+      $("#katalog").show();
+  }
+  else if(userRole == "customer"){
+      $("#katalog").show();
+      $("#purchasing").show();
+      $("#shopping_cart").show();
+  }
+
 (function () {
   if (!sessionStorage.getItem('token')) {
     if (window.location.pathname !== '/index.html' && window.location.pathname !== '/') {
@@ -24,5 +55,5 @@
     setTimeout(function (){
         window.location = 'index.html';
     }, 500);
-});
+  });  
 })();
--- src/js/home.js
+++ src/js/home.js
@@ -1,33 +1,6 @@
 (function () {
     $(document).ready(function () {
 
-        var userEmail = sessionStorage.getItem('user_email');
-        var userRole = sessionStorage.getItem('user_role');
-
-        console.log(userEmail);
-        console.log(userRole);
-
-        $(".item_menu").hide();
-
-        if(userRole == "admin"){
-            $("#vendor").show();
-            $("#katalog").show();
-            $("#purchasing").show();
-            $("#shopping_cart").show();
-            $("#kontrak").show();
-            $("#user").show();
-        }
-        else if(userRole == "vendor"){
-            $("#kontrak").show();
-            $("#vendor").show();
-            $("#katalog").show();
-        }
-        else if(userRole == "customer"){
-            $("#katalog").show();
-            $("#purchasing").show();
-            $("#shopping_cart").show();
-        }
-
         //window.location = "/katalog_produk.html";
     });
 })();
