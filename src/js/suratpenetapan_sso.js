20603db5b6c76ab59cf10029a88d51b1c4f86f05
evadantir <evadantir@gmail.com>
2020-01-20 15:32:34 +0700

    add surat penetapan vendor page for SSO

---
+++ src/js/suratpenetapan_sso.js
@@ -0,0 +1,16 @@
+function submit(){
+    window.location="vendor_on_progress_list.html"
+}
+
+$(document).ready(function () {
+    // create DateTimePicker from input HTML element
+    $("#datetimepicker").kendoDatePicker({
+        value: new Date(),
+        format: "dd/mm/yyyy",
+        dateInput: true
+    });
+
+    $("#files").kendoUpload({
+        dropZone: ".customDropZone"
+    });
+});
