16fcf679d336a5ce9b5ad6ac90d7dbb7991bb8d8
evadantir <evadantir@gmail.com>
2020-01-20 14:16:41 +0700

    remove code smells

--- src/js/detail_produk.js
+++ src/js/detail_produk.js
@@ -63,7 +63,7 @@ function lihatCart(){
                 var prodArray = JSON.parse(window.sessionStorage.getItem("shopping_cart"));
                 if(!prodArray) prodArray = {data: []};
                 res.data.Quantity = 1;
-                res.data.UnitPrice = res.data.max_price
+                res.data.UnitPrice = res.data.max_price;
                 prodArray.data.push(res.data);
                 window.sessionStorage.setItem("shopping_cart", JSON.stringify(prodArray));
             }
