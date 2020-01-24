0246fb3bc7ef415a804176d51fe9e2168284ac3b
evadantir <evadantir@gmail.com>
2020-01-24 16:23:56 +0700

    fixing procurementlist

--- src/js/procurement_list.js
+++ src/js/procurement_list.js
@@ -73,7 +73,11 @@
                     width: "280px"
                 }
             ],
-            editable: "inline"
+            dataBound: function() {
+                for (var i = 0; i < this.columns.length; i++) {
+                  this.autoFitColumn(i);
+                }
+            }
         });
 
         function showDetails(e) {
